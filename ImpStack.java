8b141be3049753a8e4a073894a0e534fdf58ff6a
zyt0328 <71568759+zyt0328@users.noreply.github.com>
2022-06-11 13:12:45 -0700

    Create ImpStack.java

---
+++ ImpStack.java
@@ -0,0 +1,33 @@
+//LeetCode 225
+class MyStack {
+    Queue<Integer> queue1;
+    Queue<Integer> queue2;
+    public MyStack() {
+        queue1 = new LinkedList<>();
+        queue2 = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        queue2.offer(x); 
+        while (!queue1.isEmpty()){
+            queue2.offer(queue1.poll());
+        }
+        Queue<Integer> queueTemp;
+        queueTemp = queue1;
+        queue1 = queue2;
+        queue2 = queueTemp;
+    }
+    
+    public int pop() {
+        return queue1.poll();
+    }
+    
+    public int top() {
+        return queue1.peek();
+    }
+    
+    public boolean empty() {
+        return queue1.isEmpty();
+    }
+}
+
